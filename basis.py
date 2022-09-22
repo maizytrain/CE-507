b063cd8328fba37ed4e976995d655778b0098313
maizytrain <jpeterson178@gmail.com>
2022-09-22 14:27:06 -0600

    First go

--- basis.py
+++ basis.py
@@ -1,15 +1,7 @@
+from string import digits
 import unittest
 import numpy
 import sympy
-
-def taylorExpansion( fun, a, order ):
-x = list( fun.atoms( sympy.Symbol ) )[0]
-t = 0
-for i in range( 0, order + 1 ):
-       df = sympy.diff( fun, x, i )
-       term = ( df.subs( x, a ) / sympy.factorial( i ) ) * ( x - a )**i
-       t += term
-return t
 
 
 def evalLegendreBasis1D( degree, variate):
@@ -27,6 +19,6 @@ def evalLegendreBasis1D( degree, variate):
 class Test_evalLegendreBasis1D( unittest.TestCase ):
        def test_linear( self ):
               for x in numpy.linspace ( -1, 1, 100):
-                     self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 1, variate = x), second = x, decimal = 10)
+                     self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 1, variate = x), second = x, digits = 7)
               
        
