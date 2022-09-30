53ad2dcc9103d6ee904691da39b3d4cde78c15ec
maizytrain <jpeterson178@gmail.com>
2022-09-30 16:48:30 -0600

    Final

--- HW 2.4 to 2.5, 8.5/Question46.py
+++ HW 2.4 to 2.5, 8.5/Question46.py
@@ -15,56 +15,68 @@ def computeNewtonCotesQuadrature( fun, num_points, degree ):
 
     calcMat = np.transpose(calcMat)
 
-    #print(calcMat)
+    xBounds = np.linspace(-1,1,num_points+1)
+    finals = 0
+    zeta = sp.symbols('zeta')
 
-    xValues = np.linspace(-1,1,degree+1)
+    for i in range(num_points):
+        eqExpr = calcSingleNewtonCotes(fun = fun, degree = degree, lowerBound = xBounds[i], upperBound = xBounds[i+1], calcMat = calcMat)
+        finals += sp.integrate(eqExpr, (zeta, xBounds[i], xBounds[i+1]))
+
+    return finals
+
+
+def calcSingleNewtonCotes( fun, degree, lowerBound, upperBound, calcMat ):
+    xValues = np.linspace(lowerBound,upperBound,degree+1)
     fValues = np.zeros(degree+1)
     for i in range(degree+1):
         fValues[i] = fun.subs(x,xValues[i])
-
-    #print(fValues)
-
+    
     iMat = np.zeros([degree+1, degree+1])
     for i in range(degree + 1):
         for j in range(degree + 1):
             iMat[i, j] = xValues[i]**(degree - j)
 
-    polyValues = (np.linalg.solve(iMat,fValues))
-    print(np.linalg.solve(calcMat,polyValues))
+    polyValues = np.linalg.solve(iMat,fValues)
+    lValues = np.linalg.solve(calcMat,polyValues)
 
-    fun = sp.Poly(fun)
-    funDegree = sp.degree(fun) + 1
-    sValues = np.zeros([funDegree, 1])
+    zeta = sp.symbols('zeta')
+    lExpr = zeta*0
+    for i in range(degree+1):
+        lExpr += lValues[i] * createLagrangeBasis1D( degree = degree, basis_idx = i)
 
-    #print(sValues)
+    sExpr = (sp.simplify(lExpr))
+
+    return sExpr
 
 
 
-    return
+
 
 x = sp.symbols('x')
-computeNewtonCotesQuadrature( fun = 2*x + 4, num_points = 1, degree = 2)
+testExpr = 4 * x**4 - x + 2
+print(computeNewtonCotesQuadrature(fun = testExpr, num_points = 5, degree = 2))
 
 class Test_computeNewtonCotesQuadrature( unittest.TestCase ):
     def test_integrate_constant_one( self ):
-        constant_one = lambda x : 1 * x**0
+        constant_one = 1 * x**0
         for degree in range( 1, 6 ):
             num_points = degree + 1
-            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = constant_one, num_points = num_points ), second = 2.0, delta = 1e-12 )
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = constant_one, num_points = num_points, degree = degree ), second = 2.0, delta = 1e-12 )
 
     def test_exact_poly_int( self ):
         for degree in range( 1, 6 ):
             num_points = degree + 1
-            poly_fun = lambda x : ( x + 1.0 ) ** degree
-            indef_int = lambda x : ( ( x + 1 ) ** ( degree + 1) ) / ( degree + 1 )
-            def_int = indef_int(1.0) - indef_int(-1.0)
-            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = poly_fun, num_points = num_points ), second = def_int, delta = 1e-12 )
+            poly_fun = ( x + 1.0 ) ** degree
+            indef_int = ( ( x + 1 ) ** ( degree + 1) ) / ( degree + 1 )
+            def_int = indef_int.subs(x,1) - indef_int.subs(x,-1)
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = poly_fun, num_points = num_points, degree = degree ), second = def_int, delta = 1e-12 )
 
     def test_integrate_sin( self ):
-        sin = lambda x : math.sin(x)
+        sin = sp.sin(x)
         for num_points in range( 1, 7 ):
-            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = sin, num_points = num_points ), second = 0.0, delta = 1e-12 )
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = sin, num_points = num_points, degree = 5 ), second = 0.0, delta = 1e-12 )
 
     def test_integrate_cos( self ):
-        cos = lambda x : math.cos(x)
-        self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = cos, num_points = 6 ), second = 2*math.sin(1), delta = 1e-4 )
+        cos = sp.cos(x)
+        self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = cos, num_points = 6, degree = 5 ), second = 2*math.sin(1), delta = 1e-4 )
